507efb2769d4e2cc3ca6bc424ed9c0466a84038c
Dr-Paganini <31491523+Dr-Paganini@users.noreply.github.com>
2017-08-31 23:17:54 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -6,8 +6,8 @@ import os
 
 
 
+#Just notes on Python
 
-#list, tuples, dictionarys,
 
 a = "December 21, 2001"
 pi_tuple = "12/21/02"
@@ -33,18 +33,72 @@ rap = {'Eminem': 'Rap God',
        'Kendrick Lemar': 'Humble',
        'Pink Guy': 'STFU',
        'Post Malone': 'Kill Yourself'}
-
+print((rap.keys()))
 print("My favorite rap song:")
 print((rap['Eminem']))
 rap['Post Malone'] = 'Congratulations'
 print((rap.get('Kendrick Lemar')))
 
-#if statements
-if len(rap) <= len(colleges):
-    print("Kill me please")
 
-if len(rap) >= len(colleges):
-    print("Qing4 sha1 le wo3")
+#if statements
+if len(rap) == len(colleges):
+    print("Kill me please")
+elif len(rap) > len(colleges):
+    print(new_list)
+else:
+    print("It's wrong my dude")
+
+if (len(rap) <= len(new_list)) and (len(rap) >= len(colleges)):
+    print("my pussy stank")
+elif (len(new_list) != len(new_tuple)) or (len(rap) != len(new_tuple)):
+    print("these are just notes bro so you can look back and ""\n"""
+          "know what everything does incase i forget")
+elif not(len(colleges) == len(rap)):
+    print("die montherfucker")
+
+#looping
+
+for x in range(0, 10):
+    print(x, " ", end=" ")
+print('\n')
+for y in colleges:
+    print(y)
+print('\n')
+for z in [2, 4, 7, 10]:
+    print(z)
+    print('\n')
+
+random_num = random.randrange(0, 100)
+
+while random_num != 21:
+    print(random_num)
+    random_num = random.randrange(0, 100)
+
+i = 0
+
+while i <= 0:
+    if i % 2 == 0:
+        print(i)
+    elif i == 9:
+        break
+    else:
+        i += 1          #i +=1 same as( i = i + 1)
+        continue
+
+        i += 1
+
+
+
+#Functions
+number = 0
+
+def add_num(fnum, lnum):
+    sum_num = fnum + lnum
+    return sumNum
+
+
+string = add_num(6, 10)
+
 
 
 
